e186dd1e098b8f8dbc71c389f2b391b93f19dbe7
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2021-08-03 17:46:37 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,16 +1,15 @@
-#!/bin/bash
-echo "Enter .pem file Name: "
-read  pemFileName
+# #!/bin/bash
+
 echo "Enter .pem file path: "
 read  pemFilePath
-echo "Enter eth2.0-deposit-cli.zip file path: "
+echo "Enter Validator Key zip file path: "
 read  zipFilePath
 echo "Enter AWS Instance Credntial: "
 read  instanceCredential
 
-chmod 400 $pemFileName
-
-scp -i $pemFilePath -r $zipFilePath $instanceCredential:/home/ec2-user
+chmod 0400 "$pemFilePath"
 
 
-ssh -i "$pemFileName" $instanceCredential
+scp -i $pemFilePath -r "$zipFilePath" $instanceCredential:/home/ec2-user
+
+ssh -i "$pemFilePath" $instanceCredential
