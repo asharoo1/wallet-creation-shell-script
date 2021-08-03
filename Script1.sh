87d975cb4ac592a6028166db65d0b7371292d5f6
asharoo1 <84122970+asharoo1@users.noreply.github.com>
2021-08-03 18:42:28 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,15 +1,15 @@
-# #!/bin/bash
+#!/bin/bash
 
 echo "Enter .pem file path: "
 read  pemFilePath
 echo "Enter Validator Key zip file path: "
 read  zipFilePath
-echo "Enter AWS public IPv4 and external DNS hostname[eg: ec2-user@ec2-1-22-333-444.compute-1.amazonaws.com]: "
+echo "Enter AWS public IPv4 and external DNS hostname[eg: ec2-user@ec2-3-87-129-138.compute-1.amazonaws.com]: "
 read  instanceCredential
 
 chmod 0400 "$pemFilePath"
 
 
-scp -i $pemFilePath -r "$zipFilePath" $instanceCredential:/home/ec2-user
+scp -i "$pemFilePath" -r "$zipFilePath" $instanceCredential:/home/ec2-user
 
 ssh -i "$pemFilePath" $instanceCredential
